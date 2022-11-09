cbb4834c06734358503350121af3b98e284dbd96
xubaba954 <117844623+xubaba954@users.noreply.github.com>
2022-11-10 00:38:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oZdt75j0LO-tt62ZlS_l-jZ1PjHc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'XDTUCao1BC3FJFAzqTnlXb2vrZumXKWIDSp9VTSTGoY',
+      useTemplateId: '6uEs_D87WXCymxpZk0TUWZwFv3w7YD7-UzKGTsE5vxI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-10',
       festivals: [
